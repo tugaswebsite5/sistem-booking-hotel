152064675ed4dd48f883cdeb33c5e60c7bf3ee92
ajengputy <ajengsalma7205@gmail.com>
2025-12-07 12:08:16 +0700

    Update pay.php

--- rooms/pay.php
+++ rooms/pay.php
@@ -1,28 +1,5 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/5c5946fe44.js" crossorigin="anonymous"></script>
-    <title>Pay Page</title>
-  </head>
-  <body>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark" >
-    <div class="container" style="margin-top: none">
-        <a class="navbar-brand  text-white" href="#">Pay Page</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      
-        </div>
-    </div>
-    </nav>
+<?php require "../includes/header.php"; ?>
+<?php require "../config/config.php"; ?>
 
     <div class="container">  
                     <!-- Replace "test" with your own sandbox Business account app client ID -->
@@ -36,7 +13,7 @@
                             return actions.order.create({
                             purchase_units: [{
                                 amount: {
-                                value: '100' // Can also reference a variable or function
+                                value: '<?php echo $_SESSION['price']; ?>' // Can also reference a variable or function
                                 }
                             }]
                             });
@@ -45,16 +22,12 @@
                         onApprove: (data, actions) => {
                             return actions.order.capture().then(function(orderData) {
                           
-                             window.location.href='index.php';
+                             window.location.href='http://localhost/sistem-booking-hotel';
                             });
                         }
                         }).render('#paypal-button-container');
                     </script>
                   
-                </div>
-            </div>
-        </div>
+     </div>
+<?php require "../includes/footer.php"; ?>
 
-
-    <body>
-</html>
