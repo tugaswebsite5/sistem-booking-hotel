817b2c60fa0dd6dc433cf7ce6c9c1f4186a16257
ajengputy <ajengsalma7205@gmail.com>
2025-12-20 10:24:37 +0700

    Create index.php

---
+++ admin-panel/index.php
@@ -0,0 +1,35 @@
+<?php require "layouts/header.php"; ?>
+            
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Hotels</h5>
+              <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
+              <p class="card-text">number of hotels: 8</p>
+             
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Rooms</h5>
+              
+              <p class="card-text">number of rooms: 4</p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Admins</h5>
+              
+              <p class="card-text">number of admins: 3</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+<?php require 'layouts/footer.php'; ?>
