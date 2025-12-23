eeb4dc62613339ab04500953ebd0e10e1488c86f
naiashaa <154899051+amourist@users.noreply.github.com>
2025-12-23 22:03:15 +0700

    Update login-admins.php

--- admin-panel/admins/login-admins.php
+++ admin-panel/admins/login-admins.php
@@ -1,7 +1,6 @@
 <?php require "../layouts/header.php"; ?>
 <?php require "../../config/config.php"; ?>
 <?php
-session_start();
 
 if(isset($_SESSION['adminname'])) {
   echo "<script>window.location.href='".ADMINURL."' </script>";
