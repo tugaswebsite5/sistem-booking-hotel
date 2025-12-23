ef602b15f50bce2a30380259e618aeccb7577d6e
naiashaa <154899051+amourist@users.noreply.github.com>
2025-12-23 22:05:44 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,31 +1,19 @@
 <?php
+session_start(); // 🔥 WAJIB INI
 
-try
-    {
-    //host
+try {
     define("HOST", "localhost");
-
-    //dbname
     define("DBNAME", "hotel-booking");
-
-    //user
     define("USER", "root");
-
-    //password
     define("PASS", "");
 
-
-    $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
+    $conn = new PDO(
+        "mysql:host=".HOST.";dbname=".DBNAME,
+        USER,
+        PASS
+    );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // if($conn ==true) {
-    //     echo "db connection is a success";
-    // } else {
-    //     echo  "error";
-    // }
-
-    } catch(PDOException $e)
-
-    {
-        echo $e->getMessage();
-    }
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
