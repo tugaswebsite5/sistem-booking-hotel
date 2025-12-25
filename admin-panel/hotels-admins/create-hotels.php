254e3e92ea8afb3bade1035ebaa11d30bf0309b6
naiashaa <154899051+amourist@users.noreply.github.com>
2025-12-25 14:52:26 +0700

    Update create-hotels.php

--- admin-panel/hotels-admins/create-hotels.php
+++ admin-panel/hotels-admins/create-hotels.php
@@ -1,11 +1,52 @@
 <?php require "../layouts/header.php"; ?>
 <?php require "../../config/config.php"; ?>
+<?php 
+
+
+if(!isset($_SESSION['adminname'])) {
+    echo "<script>window.location.href='".ADMINURL."/admins/login-admins.php' </script>";
+  }
+
+if(isset($_POST['submit'])) {
+	if(empty($_POST['name']) OR empty($_POST['description']) OR empty($_POST['location'])) {
+		echo "<script>alert('one ore more input are empty')</script>";
+	} else {
+
+
+		$name = $_POST['name'];
+		$description = $_POST['description'];
+		$location = $_POST['location'];
+    $image = $_FILES['image']['name'];
+
+    $dir = "hotel_images/" . basename($image);
+		
+		$insert = $conn->prepare("INSERT INTO hotels (name, description, location, image)
+		VALUES (:name, :description, :location)");
+		
+		$insert->execute([
+			":name" => $name,
+			":description" => $description,
+			":location" => $location,
+      ":image" => $image
+		]);
+
+    if(move_uploaded_file($_FILES['image']['tmp_name'], $dir)) {
+      header("location: show-hotels.php");
+    }
+		
+		
+
+	}
+}
+
+
+?>
        <div class="row">
         <div class="col">
           <div class="card">
             <div class="card-body">
               <h5 class="card-title mb-5 d-inline">Create Hotels</h5>
-          <form method="POST" action="" enctype="multipart/form-data">
+          <form method="POST" action="create-hotels.php" enctype="multipart/form-data">
                 <!-- Email input -->
                 <div class="form-outline mb-4 mt-4">
                   <input type="text" name="name" id="form2Example1" class="form-control" placeholder="name" />
@@ -19,7 +60,7 @@
 
                 <div class="form-group">
                   <label for="exampleFormControlTextarea1">Description</label>
-                  <textarea class="form-control" name="description" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  <textarea name="description" class="form-control" name="description" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
 
                 <div class="form-outline mb-4 mt-4">
